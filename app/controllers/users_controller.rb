3ec7334ea96e1b7b342d71989d9df73d2d07f953
aliciapflaumer <alpfla7@gmail.com>
2018-07-06 09:12:16 -0400

    Sign up as a new user
    - Edited Users controller and `users/new.html.erb`
    files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,17 +23,26 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
+  # def create
+  #   @user = User.new(user_params)
+  #
+  #   respond_to do |format|
+  #     if @user.save
+  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
+  #       format.json { render :show, status: :created, location: @user }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to root_url, notice: 'Thank you for signing up!'
+    else
+      render :new
     end
   end
 
