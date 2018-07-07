63043a291ab9188cb715027b54f9407db0f30714
aliciapflaumer <alpfla7@gmail.com>
2018-07-06 21:13:15 -0400

    Added Bootstrap classes
    - Wrapped html.erb files in container class
    - styled buttons with `btn btn-outline-primary' class

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   get 'home', to:'welcome#index', as: 'home'
+  get 'articles', to:'articles#index', as: 'articles'
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
