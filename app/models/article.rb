d3f2ae662861647ecf37c42c1b89b827d4912a45
aliciapflaumer <alpfla7@gmail.com>
2018-07-16 20:03:29 -0400

    Mount the uploader
    - Used `mount_uploader :image, ImageUploader` in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
   belongs_to :user, optional: true
   has_many :comments, dependent: :destroy
   validates :title, :author, :text, presence: true
+  mount_uploader :image, ImageUploader
 end
